b78ee77d28c316a9d6d48f1f4dec9660f8f36e9d
alviennn <alvien.rnp@gmail.com>
2024-10-18 18:03:28 +0700

    Column digunakan untuk mengatur elemen elemennya secara vertikal dan tetap berada di tengah

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -33,7 +34,11 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun BasicCompose(modifier: Modifier = Modifier) {
-    Text(
+    Column(
+
+    ) {
+
+    }
         text = "Hello $name!",
         modifier = modifier
     )
