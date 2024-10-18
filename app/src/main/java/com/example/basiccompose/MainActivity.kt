9a0d38c851d83f330ff7e3516e5a5082208a5b40
alviennn <alvien.rnp@gmail.com>
2024-10-18 18:29:53 +0700

    mengubah warna latar belakang menjadi hitam

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basiccompose.ui.theme.BasicComposeTheme
@@ -42,6 +44,7 @@ fun BasicCompose(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.spaceBy(16.dp),
         modifier = modifier
             .fillMaxSize()
+            .background(Color.Black)
     ) {
 
     }
