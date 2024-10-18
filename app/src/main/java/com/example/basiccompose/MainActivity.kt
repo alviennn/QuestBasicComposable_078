bd953ad402ff54a56e8969c634de4aaff9c88cf3
alviennn <alvien.rnp@gmail.com>
2024-10-18 18:37:08 +0700

    mengatur style dari text "Indonesia" menggunakan textsyle

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basiccompose.ui.theme.BasicComposeTheme
@@ -48,7 +49,10 @@ fun BasicCompose(modifier: Modifier = Modifier) {
             .padding(16.dp)
     ) {
         Text(
-            text = "Indonesia"
+            text = "Indonesia",
+            style = TextStyle(
+
+            )
         )
 
     }
