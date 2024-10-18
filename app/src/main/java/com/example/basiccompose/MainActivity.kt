6f1550f8e3474c4434d55a5101c93150b002abfb
alviennn <alvien.rnp@gmail.com>
2024-10-18 18:16:34 +0700

    Mengatur penyusunan elemen agar rata tengah ke bawah dan jarak antara elemen sebanyak 16 dp

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basiccompose.ui.theme.BasicComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,7 +38,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BasicCompose(modifier: Modifier = Modifier) {
     Column(
-
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spaceBy(16.dp),
     ) {
 
     }
