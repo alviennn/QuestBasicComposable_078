c92b098e40ff53ca1af1759c1d5dbe73441c2b70
alviennn <alvien.rnp@gmail.com>
2024-10-18 18:00:13 +0700

    fungsi `BasicCompose` untuk menyusun elemen UI dengan parameter `modifier` yang memungkinkan pengaturan tambahan untuk tata letak

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun BasicCompose(modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
